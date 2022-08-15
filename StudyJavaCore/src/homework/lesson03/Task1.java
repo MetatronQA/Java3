ea50200f2b8bbe25e2bce80e9fe7e9a4f804182f
ipod <me.metatron@gmail.com>
2022-08-15 19:59:26 +0300

    commit

---
+++ StudyJavaCore/src/homework/lesson03/Task1.java
@@ -0,0 +1,24 @@
+package homework.lesson03;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        invertArray();
+    }
+    private static void invertArray() {
+        int[] array = {0,1};
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] == 0 ? 1 : 0;
+        }
+        printArray(array);
+        System.out.println();
+    }
+
+    public static void  printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+}
